1143ce2453b69e4b60e76464e68e287a7c3e0882
HansM7 <melchorhans5@gmail.com>
2024-07-30 17:57:00 -0500

    cc

--- src/context/environment.ts
+++ src/context/environment.ts
@@ -1,8 +1,8 @@
 const dev = false;
 export const base_backend = dev
   ? "http://localhost:4000"
-  : "http://161.132.48.124:4000";
+  : "http://161.132.47.218:4000";
 
 export const base_frontend = dev
   ? "http://localhost:3000"
-  : "http://161.132.48.124:4000";
+  : "http://161.132.47.218:4000";
